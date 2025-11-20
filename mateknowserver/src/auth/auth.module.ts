a79e62025fa141956b8a135d2ceffd234738c80d
SimonOcampo1 <simontadeoocampo@alu.frlp.utn.edu.ar>
2025-11-20 03:55:33 -0300

    fix(server/auth): ajustar configuración del módulo de autenticación

--- mateknowserver/src/auth/auth.module.ts
+++ mateknowserver/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SupabaseService } from '../lib/supabase.service';
 import { AuthGuard } from './guards/auth.guard';
 
+@Global()
 @Module({
   controllers: [AuthController],
   providers: [AuthService, SupabaseService, AuthGuard],
